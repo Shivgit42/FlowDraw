bd5df6ffeb335e1a29ad1bd1a80e925831735333
Shivam Rana <4826shivam@gmail.com>
2025-09-14 18:17:04 +0530

    auth middleware

--- apps/http-server/src/middleware/auth.middleware.ts
+++ apps/http-server/src/middleware/auth.middleware.ts
@@ -1,29 +1,41 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-interface DecodedToken extends JwtPayload{
-    id: string;
-    email: string;
+interface DecodedToken extends JwtPayload {
+  id: string;
+  email: string;
 }
-export const protectRoute = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const token = req.cookies["__Secure-next-auth.session-token"] || req.cookies["next-auth.session-token"];
-        
-        if(!token){
-            res.status(401).json({message: "JWT must be provided"});
-            return
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
-        
-        if(typeof decoded !== "object" || !decoded.id){
-            res.status(401).json({ message: "Invalid token" })
-            return
-        }
-        req.user = decoded;
-        next();
-    } catch (error) {
-        console.log(error);
-        res.status(401).json({message: "Unauthorized"});
-        return
+export const protectRoute = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token =
+      authHeader && authHeader.startsWith("Bearer ")
+        ? authHeader.split(" ")[1]
+        : req.cookies["__Secure-next-auth.session-token"] ||
+          req.cookies["next-auth.session-token"];
+
+    if (!token) {
+      res.status(401).json({ message: "JWT must be provided" });
+      return;
     }
-}
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as DecodedToken;
+
+    if (typeof decoded !== "object" || !decoded.id) {
+      res.status(401).json({ message: "Invalid token" });
+      return;
+    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+};
