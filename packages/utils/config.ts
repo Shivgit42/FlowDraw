c39696e03df52a76f659d89a156da19322b384f4
Shivam Rana <4826shivam@gmail.com>
2025-09-14 16:24:36 +0530

    api url fixing

--- packages/utils/config.ts
+++ packages/utils/config.ts
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
-export const SOCKET_URL =
-  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL!;
