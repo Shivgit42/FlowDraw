5463a0af44abf874afe31f371aa7a5c24dab44bd
Shivam Rana <4826shivam@gmail.com>
2025-09-14 14:07:48 +0530

    api fixing

--- packages/utils/config.ts
+++ packages/utils/config.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://flowdraw.shivamte.me/api";
 export const SOCKET_URL =
   process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002";
